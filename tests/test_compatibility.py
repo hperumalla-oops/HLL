1394f6f3687243fbf74da00de145e5904376d505
Shrxyxs <156272857+Shrxyxs@users.noreply.github.com>
2025-07-26 17:50:13 +0530

    Create test_compatibility.py

---
+++ tests/test_compatibility.py
@@ -0,0 +1,12 @@
+import unittest
+from hyperloglog.core import HyperLogLog
+
+class TestCompatibility(unittest.TestCase):
+    def test_string_and_bytes(self):
+        hll = HyperLogLog(b=14)
+        hll.add("foo")
+        hll.add(b"foo")  # Should not raise, but may hash differently
+        self.assertIsInstance(hll.estimate(), float)
+
+if __name__ == "__main__":
+    unittest.main()
