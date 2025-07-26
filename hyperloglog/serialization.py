9db9b8d14a55b6ea8f2f66a73b749dd677dfc7c0
hperumalla-oops <144527455+hperumalla-oops@users.noreply.github.com>
2025-07-26 16:43:33 +0530

    Update and rename serialize.py to serialization.py

---
+++ hyperloglog/serialization.py
@@ -0,0 +1,12 @@
+import base64
+import pickle
+
+def serialize_hll(hll_obj):
+    """Serialize HyperLogLog to base64 string"""
+    binary = pickle.dumps(hll_obj)
+    return base64.b64encode(binary).decode('ascii')
+
+def deserialize_hll(b64_data):
+    """Deserialize HyperLogLog from base64 string"""
+    binary = base64.b64decode(b64_data)
+    return pickle.loads(binary)
