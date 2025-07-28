2005d59fb96a0a7ebc9b932d92b64aea25b21701
Suravi <suravireddy21@gmail.com>
2025-07-28 07:39:50 +0530

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='hyperloglog',
+    version='1.0',
+    description='An implementation of HyperLogLog with sparse and dense modes',
+    author='RV AIML Dep',
+    packages=find_packages(),  
+    install_requires=[
+        psycopg2==2.9.10
+    ],
+    python_requires='>=3.6',
+)
