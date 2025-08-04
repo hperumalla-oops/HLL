6dfa94eefbd8dd09b3b8123614ebb3e48fb1e038
Suravi <suravireddy21@gmail.com>
2025-08-04 09:32:23 +0530

    Update core.py

--- hyperloglog/core.py
+++ hyperloglog/core.py
@@ -1,37 +1,48 @@
 from .dense import DenseHyperLogLog
 from .sparse import hll_estimate_sparse, dedupe_and_sort, SparseHyperLogLog
-from .compression import pack_registers, unpack_registers
+from .compression import pack_registers
 
 class HyperLogLog:
-    def __init__(self, b=14, mode='dense'):
-        self.b = b
-        self.mode = mode
+    def __init__(self, b=14, mode='sparse', register=0):
+        self.b = b # number of bits in the register
+        self.mode = mode # dense or sparse 
+        self.m = 1 << b
         self.dontbefore = False
         if mode == 'dense':
-            self.impl = DenseHyperLogLog(b)
+            self.impl = DenseHyperLogLog(b, register)
         elif mode == 'sparse':
-            self.impl = SparseHyperLogLog(b)           
+            self.impl = SparseHyperLogLog(b, register)           
         else:
             raise ValueError('Unknown mode: ' + str(mode))
+        self.registers = self.impl.registers
 
     def add(self, item):
-        self.impl.add(item)
+        if (self.impl.add(item)):
+            print("Converting to Dense")
+            registers = [0] * self.m
+            for idx, rho in self.registers:
+                registers[idx] = rho
+            self.registers = registers
+            self.convert_to_dense()
 
     def estimate(self):
         return self.impl.estimate()
 
     def storing(self):
-        return pack_registers(self.impl.registers, 14)
+        return pack_registers(self.registers, 14)
 
-    def decompress(self, packed_registers): # takes the output of storing
-         unpack_registers(packed_registers, 1 << self.b, 14)
+    def convert_to_dense(self):
+        self.mode = 'dense'
+        self.impl = DenseHyperLogLog(self.b, self.storing())
+        self.registers = self.impl.registers
 
-        
     def merge(self, hll2):
         if self.b != hll2.b:
             raise ValueError("Cannot merge HLLs with different precision (b) values")
         if self.mode == 'sparse':
+            print("Converting to Dense")
             self._convert_to_dense()
+
         if hll2.mode == 'sparse':
             hll2._convert_to_dense()
                               
