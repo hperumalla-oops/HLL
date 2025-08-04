fd3b732d8c33bf003c8b19d75c7ea9523f7bd47e
Suravi <suravireddy21@gmail.com>
2025-08-04 09:31:07 +0530

    Update sparse.py

--- hyperloglog/sparse.py
+++ hyperloglog/sparse.py
@@ -4,6 +4,8 @@ import math
 from .constants import ALPHA_MM, THRESHOLD, rawEstimateData, biasData
 from .bias_correction import bias_estimate
 from .hash_utils import murmurhash64a
+from .compression import unpack_registers
+
 
 def dedupe_and_sort(sparse_data):
     """
@@ -45,33 +47,33 @@ def hll_estimate_sparse(hloglog_b, hloglog_binbits, sparse_data, sparse_idx):
 
 
 class SparseHyperLogLog:
-    def __init__(self, b=14, sparse_threshold=None):
+    def __init__(self, b=14, register=0, sparse_threshold=None):
         self.b = b
         self.m = 1 << b
         self.sparse_threshold = sparse_threshold or (self.m // 4)
-        self.sparse_registers = []
-        self.is_sparse = True
+        if register:
+            self.registers = unpack_registers(register, 1 << b, b)
+        self.registers = []
         
     def add(self, item):
-        if not self.is_sparse:
-            return self._dense_add(item)
-            
+           
         hash_value = murmurhash64a(item)
         idx = hash_value >> (64 - self.b)
         w = (hash_value << self.b) & ((1 << 64) - 1)
         rho = self._rho(w, 64 - self.b)
         
-        pos = bisect_left(self.sparse_registers, (idx, 0))
+        pos = bisect_left(self.registers, (idx, 0))
         
-        if pos < len(self.sparse_registers) and self.sparse_registers[pos][0] == idx:
-            current_rho = self.sparse_registers[pos][1]
+        if pos < len(self.registers) and self.registers[pos][0] == idx:
+            current_rho = self.registers[pos][1]
             if rho > current_rho:
-                self.sparse_registers[pos] = (idx, rho)
+                self.registers[pos] = (idx, rho)
         else:
-            self.sparse_registers.insert(pos, (idx, rho))
+            self.registers.insert(pos, (idx, rho))
             
-        if len(self.sparse_registers) > self.sparse_threshold:
-            self._convert_to_dense()
+        if len(self.registers) > self.sparse_threshold:
+            return 1
+        return 0
     
     def _rho(self, w, max_bits):
         def clzll(x): return 64 - x.bit_length() if x != 0 else 64
@@ -83,60 +85,17 @@ class SparseHyperLogLog:
                 rho += addn
         return rho
     
-    def _convert_to_dense(self):
-        """Convert from sparse to dense representation"""
-        self.registers = [0] * self.m
-        
-        for idx, rho in self.sparse_registers:
-            self.registers[idx] = rho
-            
-        # self.sparse_registers = []
-        self.is_sparse = False
-    
-    def _dense_add(self, item):
-        """Add item using dense representation (after conversion)"""
-        hash_value = murmurhash64a(item)
-        idx = hash_value >> (64 - self.b)
-        w = (hash_value << self.b) & ((1 << 64) - 1)
-        rho = self._rho(w, 64 - self.b)
-        self.registers[idx] = max(self.registers[idx], rho)
     
     def estimate(self):
-        if self.is_sparse:
-            return self._sparse_estimate()
-        else:
-            return self._dense_estimate()
-    
-    def _sparse_estimate(self):
         """Estimate cardinality using sparse representation"""
         m = self.m
         
-        Z = m - len(self.sparse_registers)  # contribution from zero registers
-        Z += sum(2.0 ** -rho for _, rho in self.sparse_registers)
+        Z = m - len(self.registers)  # contribution from zero registers
+        Z += sum(2.0 ** -rho for _, rho in self.registers)
         
         E = ALPHA_MM[self.b] / Z
         
-        V = m - len(self.sparse_registers)
-        
-        if E <= THRESHOLD[self.b]:
-            correction = bias_estimate(E, self.b)
-            E -= correction
-            if E < 0:
-                E = 0
-        
-        if V > 0:
-            H = m * math.log(m / V)
-            if H <= THRESHOLD[self.b]:
-                return H
-                
-        return E
-    
-    def _dense_estimate(self):
-        """Estimate cardinality using dense representation (after conversion)"""
-        m = self.m
-        Z = sum(2.0 ** -r for r in self.registers)
-        E = ALPHA_MM[self.b] / Z
-        V = self.registers.count(0)
+        V = m - len(self.registers)
         
         if E <= THRESHOLD[self.b]:
             correction = bias_estimate(E, self.b)
