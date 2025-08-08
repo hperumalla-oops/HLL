96bca30449587e6f1aeaefcdd86d1938acf6b177
Suravi <suravireddy21@gmail.com>
2025-08-08 18:07:08 +0530

    Update serialization.py

--- hyperloglog/serialization.py
+++ hyperloglog/serialization.py
@@ -1,12 +1,12 @@
 import base64
 import pickle
 
-def serialize_hll(hll_obj):
+def serialize_hll(hll_obj: object) -> str:
     """Serialize HyperLogLog to base64 string"""
     binary = pickle.dumps(hll_obj)
     return base64.b64encode(binary).decode('ascii')
 
-def deserialize_hll(b64_data):
+def deserialize_hll(b64_data: str) -> object:
     """Deserialize HyperLogLog from base64 string"""
     binary = base64.b64decode(b64_data)
     return pickle.loads(binary)
