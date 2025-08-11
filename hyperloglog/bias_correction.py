c4c0f1db4fca39621765261a7987929907eea55f
hperumalla-oops <144527455+hperumalla-oops@users.noreply.github.com>
2025-08-11 14:41:15 +0530

    Update bias_correction.py

--- hyperloglog/bias_correction.py
+++ hyperloglog/bias_correction.py
@@ -1,25 +1,16 @@
-from bisect import bisect_left
 from .constants import rawEstimateData, biasData
 
+from numpy import searchsorted
+
 def bias_estimate(E: float, b: int) -> float:
-    """
-    Interpolates the bias correction for a given estimate value.
-    Args:
-        E: float - the raw estimate value for which bias correction is needed
-        b: int - index into precomputed bias and raw estimate tables
-    Returns:
-        float: the interpolated bias correction value
-    """
     raw = rawEstimateData[b]
     bias = biasData[b]
-    idx = bisect_left(raw, E)
+    idx = searchsorted(raw, E)
     if idx == 0:
         return bias[0]
     elif idx == len(raw):
         return bias[-1]
     else:
-        x0, x1 = raw[idx-1], raw[idx]
-        y0, y1 = bias[idx-1], bias[idx]
-        return y0 + (E - x0) * (y1 - y0) / (x1 - x0)
+        return bias[idx-1] + (E - raw[idx-1]) * (bias[idx] - bias[idx-1]) / (raw[idx] - raw[idx-1])
 
 
