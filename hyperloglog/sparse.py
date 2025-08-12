93d38b64f62764c0a96c3b6714f7507a1301ea83
hperumalla-oops <144527455+hperumalla-oops@users.noreply.github.com>
2025-08-12 21:02:00 +0530

    Update sparse.py

--- hyperloglog/sparse.py
+++ hyperloglog/sparse.py
@@ -8,35 +8,6 @@ from .compression import decompress_sparse_registers
 
 
 
-def hll_estimate_sparse(hloglog_b: int, hloglog_binbits: int, sparse_data: list[int], sparse_idx: int) -> float:
-    """
-    Evaluates the stored encoded hashes using linear counting.
-    Args:
-        hloglog_b: The 'b' parameter (number of index bits)
-        hloglog_binbits: Number of bits per bin
-        sparse_data: List of encoded hash values
-        sparse_idx: Number of elements in sparse_data
-    Returns:
-        Cardinality estimate using linear counting
-    """
-    
-    m = 2 ** (32 - 1 - hloglog_binbits)
-    sparse_idx, sparse_data_deduped = dedupe_and_sort(sparse_data[:sparse_idx])
-    V = 0
-    
-    for i in range(sparse_idx):
-        if i == 0:
-            V += 1
-            
-        elif sparse_data_deduped[i] != sparse_data_deduped[i-1]:
-            V += 1
-            
-    V = sparse_idx
-    if m - V <= 0:
-        return float('inf')
-        
-    estimate = m * math.log(m / (m - V))
-    return estimate
 
 
 class SparseHyperLogLog:
