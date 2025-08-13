71b6c08c9329c998dd3f41b4328c62ceeb464f86
hperumalla-oops <144527455+hperumalla-oops@users.noreply.github.com>
2025-08-14 01:42:41 +0530

    Add files via upload

---
+++ tests/test_add.py
@@ -0,0 +1,34 @@
+
+import sys
+from pathlib import Path
+sys.path.insert(0, str(Path(__file__).resolve().parents[1]))  # repo root
+
+from hyperloglog.core import HyperLogLog
+from hyperloglog.sparse import SparseHyperLogLog
+import unittest
+from hyperloglog.dense import DenseHyperLogLog
+
+
+class TestCoreAdd(unittest.TestCase):
+
+    def test_add_stays_sparse_below_threshold(self):
+        hll = HyperLogLog(b=14)  # starts sparse threshhold= m//4 for 14 it is 4096
+        sparse_threshold = hll.impl.sparse_threshold
+
+        for i in range(sparse_threshold):
+            hll.add(f"item-{i}")
+
+        self.assertIsInstance(hll.impl, SparseHyperLogLog)
+
+    def test_add_converts_to_dense_above_threshold(self):
+        hll = HyperLogLog(b=14)  # starts sparse
+        sparse_threshold = hll.impl.sparse_threshold
+
+        for i in range(sparse_threshold + 1002):  # random more than threshold
+            hll.add(f"item-{i}")
+
+        self.assertIsInstance(hll.impl, DenseHyperLogLog)
+
+
+if __name__ == '__main__':
+    unittest.main()
