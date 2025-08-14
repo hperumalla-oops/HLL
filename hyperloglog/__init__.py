8bd619715057d84900cdf7fa6b805534f57295f8
Suravi <suravireddy21@gmail.com>
2025-08-14 16:01:07 +0530

    Update __init__.py

--- hyperloglog/__init__.py
+++ hyperloglog/__init__.py
@@ -1,5 +1,15 @@
 """
-HyperLogLog package init.
+HyperLogLog package initializer.
+
+This package provides:
+- HyperLogLog: The main class for cardinality estimation.
+- serialize_hll / deserialize_hll: Safe Base64 serialization utilities.
 """
 from .core import HyperLogLog
+from .serialization import serialize_hll, deserialize_hll
 
+__all__ = [
+    "HyperLogLog",
+    "serialize_hll",
+    "deserialize_hll",
+]
