1032778877fb47f9cc9c3bae58575fcaaccb799b
hperumalla-oops <144527455+hperumalla-oops@users.noreply.github.com>
2025-08-17 22:49:26 +0530

    Update core.py

--- hyperloglog/core.py
+++ hyperloglog/core.py
@@ -80,8 +80,6 @@ class HyperLogLog:
         Converts the current HLL from sparse to dense mode.
         Updates internal implementation and registers.
         """
-        from hyperloglog.dense import DenseHyperLogLog
-
         # Build dense registers from sparse data
         if self.mode == 'sparse':
             if isinstance(self.impl, SparseHyperLogLog):
@@ -212,6 +210,7 @@ class HyperLogLog:
         return cls.from_bytes(data)
         
         
+
 
 
 
